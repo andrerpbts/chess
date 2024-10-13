1b746b75c2fc4b48562719122cff18bf153759f2
André Rodrigues <andrerpbts@gmail.com>
2024-10-13 19:12:42 -0300

    Alphabetize pieces methods

--- lib/chess/pieces/knight.rb
+++ lib/chess/pieces/knight.rb
@@ -1,8 +1,8 @@
 module Chess
   module Pieces
     class Knight < Piece
-      def white_symbol = '♘'
       def black_symbol = '♞'
+      def white_symbol = '♘'
     end
   end
 end
--- lib/chess/pieces/pawn.rb
+++ lib/chess/pieces/pawn.rb
@@ -1,8 +1,8 @@
 module Chess
   module Pieces
     class Pawn < Piece
-      def white_symbol = '♙'
       def black_symbol = '♟'
+      def white_symbol = '♙'
     end
   end
 end
