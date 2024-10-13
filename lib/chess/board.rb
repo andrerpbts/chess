59ff4c9b2ae695930e3c6b1e85dded1ce4eb694e
André Rodrigues <andrerpbts@gmail.com>
2024-10-13 19:24:44 -0300

    Draw CHESS banner

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -33,18 +33,18 @@ module Chess
     end
 
     def draw_rank_spacing_row(rank_number)
-      puts '    ' + 8.times.map { |n| cycled(rank_number, n, '       ') }.join + reset
+      puts '      ' + 8.times.map { |n| cycled(rank_number, n, '       ') }.join + reset
     end
 
     def draw_rank_pieces_row(rank_number, rank)
       files = rank.map.with_index { |s, n| cycled(rank_number, n, "   #{s.piece}   ") }.join
 
-      puts "#{rank_number}   " + files + reset
+      puts "  #{rank_number}   " + files + reset
     end
 
     def draw_files_labels
       puts
-      puts '    ' + ('a'..'h').map { |file_letter| "   #{file_letter}   " }.join + reset
+      puts '      ' + ('a'..'h').map { |file_letter| "   #{file_letter}   " }.join + reset
     end
 
     def cycled(rank_number, n, str)
